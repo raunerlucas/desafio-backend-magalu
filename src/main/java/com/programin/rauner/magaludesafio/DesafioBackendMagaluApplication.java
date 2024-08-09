09832505c2bf3895d610f2a51f967df6a64fcbdf
raunerlucas <raunerlucas@gmail.com>
2024-08-09 19:37:00 -0300

    Init project

---
+++ src/main/java/com/programin/rauner/magaludesafio/DesafioBackendMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.programin.rauner.magaludesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendMagaluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioBackendMagaluApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programin/rauner/magaludesafio/DesafioBackendMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programin.rauner.magaludesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendMagaluApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
