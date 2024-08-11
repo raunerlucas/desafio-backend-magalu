94d5edb9c1b15d4e709541556f1e7d2131c0af18
raunerlucas <raunerlucas@gmail.com>
2024-08-11 16:59:59 -0300

    feat: find by Notifications id.

--- src/main/java/com/programin/rauner/magaludesafio/controller/NotificationsController.java
+++ src/main/java/com/programin/rauner/magaludesafio/controller/NotificationsController.java
@@ -2,13 +2,13 @@ package com.programin.rauner.magaludesafio.controller;
 
 
 import com.programin.rauner.magaludesafio.controller.DTO.ScheduleNotificationsDto;
+import com.programin.rauner.magaludesafio.model.entity.Notifications;
 import com.programin.rauner.magaludesafio.model.service.NotificationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController()
 @RequestMapping("/notifications")
@@ -18,9 +18,14 @@ public class NotificationsController {
     private NotificationsService notificationsService;
 
     @PostMapping()
-    public ResponseEntity<?> scheduleNotifications(@RequestBody ScheduleNotificationsDto dto) {
-        notificationsService.scheduleNotifications(dto);
-        return ResponseEntity.accepted().build();
+    public ResponseEntity<Void> scheduleNotifications(@RequestBody ScheduleNotificationsDto dto) {
+        var id = notificationsService.scheduleNotifications(dto);
+        return ResponseEntity.accepted().location(URI.create("/"+id)).build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Notifications> getNotification(@PathVariable Long id) {
+        return notificationsService.getNotification(id)
+         .map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/programin/rauner/magaludesafio/model/service/NotificationsService.java
+++ src/main/java/com/programin/rauner/magaludesafio/model/service/NotificationsService.java
@@ -1,9 +1,12 @@
 package com.programin.rauner.magaludesafio.model.service;
 
 import com.programin.rauner.magaludesafio.controller.DTO.ScheduleNotificationsDto;
+import com.programin.rauner.magaludesafio.model.entity.Notifications;
 import com.programin.rauner.magaludesafio.model.repository.NotificationsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class NotificationsService {
@@ -15,8 +18,11 @@ public class NotificationsService {
         this.notificationsRepository = notificationsRepository;
     }
 
-    public void scheduleNotifications(ScheduleNotificationsDto dto) {
-        notificationsRepository.save(dto.toNotifications());
+    public Long scheduleNotifications(ScheduleNotificationsDto dto) {
+        return notificationsRepository.save(dto.toNotifications()).getId();
+    }
 
+    public Optional<Notifications> getNotification(Long id) {
+        return notificationsRepository.findById(id);
     }
 }
